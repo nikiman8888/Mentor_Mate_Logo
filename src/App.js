e28a8dd70755f7365fc8989b01636f1f24a3307b
Nikolay <nikiman88@yahoo.com>
2020-02-17 20:27:17 +0200

    Configurate dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {BrowserRouter} from 'react-dom';
 import './App.css';
 
 function App() {
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navigation.css';
+
+
+function Navigation (){
+    return (
+        <h1>Menor Mate Logo Test</h1>
+    )
+}
+export default Navigation;
