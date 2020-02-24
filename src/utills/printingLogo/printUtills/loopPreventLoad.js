f3a367bff472d8bd23b6a7b5f47b9f6e916be10f
Nikolay <nikiman88@yahoo.com>
2020-02-24 19:25:48 +0200

    Refactorinig and add loopPreventLoad function

--- src/utills/printingLogo/printUtills/loopPreventLoad.js
+++ src/utills/printingLogo/printUtills/loopPreventLoad.js
@@ -1,21 +1,42 @@
 import drawer from './drawer';
 let result = [];
-let arrayContainer = [];
+let result2 = [];
 const line = '-';
 const star = '*';
 
-function loopPreventLoad(number,row,count) {
-       
-   for (let loopRow = row; loopRow < count; loopRow++) {
-       arrayContainer =
-       drawer(+number - loopRow, line) +
-       drawer(+number + loopRow * 2, star) +
-       drawer(+number - loopRow * 2, line) +
-       drawer(+number + loopRow * 2, star) +
-       drawer(+number - loopRow, line);
-       result.push(arrayContainer);
-   }
-   return result;
+function loopPreventLoadUpperPart(number, row, count) {
+    let arrayContainer = [];
+    for (let loopRow = row; loopRow < count; loopRow++) {
+        arrayContainer =
+            drawer(+number - loopRow, line) +
+            drawer(+number + loopRow * 2, star) +
+            drawer(+number - loopRow * 2, line) +
+            drawer(+number + loopRow * 2, star) +
+            drawer(+number - loopRow, line);
+        result.push(arrayContainer);
+    }
+    let copyResult = result;
+    result = [];
+    return copyResult; // send copy and clear result for future inputs
 }
 
-export default loopPreventLoad;
+function loopPreventLoadLowerPart(number, row, count) {
+    let arrayContainer = [];
+    for (let loopRow = row; loopRow < count; loopRow++) {
+        arrayContainer =
+            drawer((+number - 1) / 2 - loopRow, line) +
+            drawer(+number, star) +
+            drawer(loopRow * 2 + 1, line) +
+            drawer(((+number * 2) - 1) - loopRow * 2, star) +
+            drawer(loopRow * 2 + 1, line) +
+            drawer(+number, star) +
+            drawer((+number - 1) / 2 - (loopRow), line);
+        result2.push(arrayContainer);
+        debugger;
+    }
+    let copyResult2 = result2;
+    result2 = [];
+    return copyResult2; // send copy and clear result for future inputs
+}
+
+export  { loopPreventLoadUpperPart,loopPreventLoadLowerPart };
--- src/utills/printingLogo/printUtills/printingLogo.js
+++ src/utills/printingLogo/printUtills/printingLogo.js
@@ -1,24 +1,31 @@
-import loopPreventLoad from './loopPreventLoad';
-import drawer from './drawer';
+import {loopPreventLoadUpperPart,loopPreventLoadLowerPart} from './loopPreventLoad';
 
 let logoPart1 = [];
 let logoPart2 = [];
-const star = '*';
-const line = '-'
+let currentRowLetter1;
+let currentRowLetter2;
 
 // printing the upper part of logo
 function printingLogoUpPart(number) {
     const halfRowsLogo = (+number + 1) / 2;
 
-    for (let row = 0; row < halfRowsLogo; row++) { // += 100 because use preventLoad function ??
-        let currentRowLetter1 =
-
-            drawer(+number - row, line) +
-            drawer(+number + row * 2, star) +
-            drawer(+number - row * 2, line) +
-            drawer(+number + row * 2, star) +
-            drawer(+number - row, line);
-        logoPart1.push(currentRowLetter1+currentRowLetter1);//concat two M letters upper part per row
+    for (let row = 0; row < halfRowsLogo; row += 100) { 
+        
+        if(halfRowsLogo - row < 100){
+            // if input is less than 100 or printing last rows less than 100
+            currentRowLetter1  = loopPreventLoadUpperPart(+number,row,halfRowsLogo-row);
+            
+        }else{
+            // if input is more then 100 we looping to 100 to prevent long loop loading
+            // looPreventLoad function is something like midlleware to make less presure ,when input number is bigger
+            currentRowLetter1  = loopPreventLoadUpperPart(+number,row,100);
+            
+        }
+           // the returned array we transform in string in prepare for the component rendering
+        for (let i = 0; i < currentRowLetter1.length; i++) {
+           let resultRow = currentRowLetter1[i] + currentRowLetter1[i];
+           logoPart1.push(resultRow);   
+        }
     }
 
     let result = logoPart1;         // copy the array
@@ -31,17 +38,24 @@ function printingLogoDownPart(number) {
 
     const halfRowsLogo = (number + 1) / 2;
 
-    for (let row = 0; row < halfRowsLogo; row++) {
-        let currentRowLetter2 =
-
-            drawer((number - 1) / 2 - row, line) +
-            drawer(number, star) +
-            drawer(row * 2 + 1, line) +
-            drawer(((number * 2) - 1) - row * 2, star) +
-            drawer(row * 2 + 1, line) +
-            drawer(number, star) +
-            drawer((number - 1) / 2 - (row), line);
-        logoPart2.push(currentRowLetter2+currentRowLetter2); //concat two M letters lower part per row
+    for (let row = 0; row < halfRowsLogo; row+=100) {
+        if(halfRowsLogo - row < 100){
+            // if input is less than 100 or printing last rows less than 100
+            currentRowLetter2  = loopPreventLoadLowerPart(+number,row,halfRowsLogo-row);
+            debugger;
+            
+        }else{
+            // if input is more then 100 we looping to 100 to prevent long loop loading
+            // looPreventLoad function is something like midlleware to make less presure ,when input number is bigger
+            currentRowLetter2  = loopPreventLoadLowerPart(+number,row,100);
+            
+        }
+        for (let i = 0; i < currentRowLetter1.length; i++) {
+            let resultRow = currentRowLetter2[i] + currentRowLetter2[i];
+            logoPart2.push(resultRow);   
+         }
+        
+        //logoPart2.push(currentRowLetter2+currentRowLetter2); //concat two M letters lower part per row
     }
 
     let result = logoPart2; // copy the array
